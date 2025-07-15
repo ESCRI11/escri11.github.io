a96c80f405665f1223961be715d825aebf9ba91b
ESCRI11 <xavier.escriba.montagut@gmail.com>
2025-07-15 14:06:50 +0200

    typo

--- script.js
+++ script.js
@@ -149,7 +149,7 @@ const content = {
       {
         "title": "PLAID: ultrafast single-sample gene set enrichment scoring",
         "authors": "Zito, A., ..., Escriba-Montagut, X., ... & Kwee, I.",
-        "journal": "boiRxiv",
+        "journal": "bioRxiv",
         "year": 2025,
         "doi": "https://doi.org/10.1101/2025.06.14.659661"
       },
